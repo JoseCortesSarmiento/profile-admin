6c6ecd1af1b33d9678a20b6c6a35dbd0d63311b6
root <root@localhost.localdomain>
2021-03-23 23:51:22 -0600

    create form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import CreateForm from '../src/components/CreateForm'
 
 function App() {
   return (
     <div className="App">
-      
+      <CreateForm></CreateForm>
     </div>
   );
 }
